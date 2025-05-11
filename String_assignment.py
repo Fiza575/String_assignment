05e58f72e8d3e0b6c292cf8495b822415f02e188
Fiza Arzoo Khalid <shaizakanwal025@gmail.com>
2025-05-11 17:07:41 +0500

    Add files via upload

---
+++ String_assignment.py
@@ -0,0 +1,47 @@
+# Task 1 (Characters)
+
+letter = "A"
+number = "7"
+symbol = "#"
+print(letter,number,symbol)
+
+# Task 2 (Strings)
+
+favorite_animal = "Rabbit"
+about_favorite_animal ="Rabbits are cute"
+print(favorite_animal)
+print(about_favorite_animal)
+
+# Task 3 (Concatenation)
+
+first_name ="Fiza Arzoo"
+last_name = "Khalid"
+print(first_name+" "+last_name)
+
+# Task 4 (Repetition)
+
+greet = "Hi"
+print(greet*5)
+
+# Task 5 (indexing)
+
+message = "PythonRocks"
+print(message[1])
+print(message[4])
+print(message[10])
+
+# Task 6 (Slicing)
+
+coding_love = "I love coding in Python"
+print(coding_love[2:6])
+print(coding_love[17:23])
+
+
+# Bonus Task 
+name = "Fiza Arzoo Khalid"
+welcome ="Welcome," + name +"!"
+message = welcome + "Let's begin your amazing journey"
+print(message)
+
+
+
